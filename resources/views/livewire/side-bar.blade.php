166c04578d879b181e06c62979858f1fe4df3af7
mwagenipecy <percyegno@gmail.com>
2024-07-13 15:55:47 +0300

    backend layout

--- resources/views/livewire/side-bar.blade.php
+++ resources/views/livewire/side-bar.blade.php
@@ -192,7 +192,18 @@
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25"></path>
                         </svg>
-                        <span class="text-base  hover:text-[#F5AD42] text-white font-bold ml-3">Logout</span>
+
+
+                        <form method="POST" action="{{ route('logout') }}" x-data>
+                            @csrf
+
+                            <i href="{{ route('logout') }}"
+                                             @click.prevent="$root.submit();">
+                                             <span class="text-base  hover:text-[#F5AD42] text-white font-bold ml-3">Logout</span>
+
+                        </i>
+                        </form>
+
                     </div>
 
                     <div class="flex shrink-0 ml-2 duration-200"></div>
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,19 +1,35 @@
-
-
-<nav x-data="{ open: false }" class="bg-white mx-1  top-0  shadow-lg dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+<nav x-data="{ open: false }" class="bg-white mx-1 ml-60 h-20 top-0 shadow-lg dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-full">
-        <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
+        <div class="flex justify-between h-16 mt-4 mx-4">
+            <div class="flex  w-2/3">
 
-                <!-- Navigation Links -->
+                <form class="w-full mx-full">
+                    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                    <div class="relative">
+                        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                            </svg>
+                        </div>
+                        <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-50 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+                    </div>
+                </form>
+
             </div>
 
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
+            <div class="flex items-center space-x-6 border-r">
+                <!-- Notification Icon -->
+                <div class="relative ">
+                    <button class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none">
+                        <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4.05-5.658A2 2 0 1010 6.342 6.002 6.002 0 006 11v3.159c0 .53-.21 1.04-.595 1.436L4 17h5m0 0v1a3 3 0 006 0v-1m-6 0h6"></path>
+                        </svg>
+                    </button>
+                </div>
 
                 <!-- Settings Dropdown -->
-                <div class="ms-3 relative">
+                <div class="relative">
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
@@ -23,6 +39,10 @@
                             @else
                                 <span class="inline-flex rounded-md">
                                     <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
+                                            <i class="text-gray-500 mx-4  dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none">
+                                                <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                                            </i>
+
                                         {{ Auth::user()->name }}
 
                                         <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
@@ -36,12 +56,8 @@
                         <x-slot name="content">
                             <!-- Account Management -->
                             <div class="block px-4 py-2 text-xs text-gray-400">
-
                                 {{ Auth::user()->name }}
                             </div>
-
-
-
 
                             <div class="border-t border-gray-200 dark:border-gray-600"></div>
 
@@ -50,7 +66,7 @@
                                 @csrf
 
                                 <x-dropdown-link href="{{ route('logout') }}"
-                                         @click.prevent="$root.submit();">
+                                                 @click.prevent="$root.submit();">
                                     {{ __('Log Out') }}
                                 </x-dropdown-link>
                             </form>
@@ -71,16 +87,11 @@
         </div>
     </div>
 
-
-
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-
-
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
             <div class="flex items-center px-4">
-
                 <div>
                     <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
                     <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
@@ -88,22 +99,16 @@
             </div>
 
             <div class="mt-3 space-y-1">
-
-
-
                 <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}" x-data>
                     @csrf
 
                     <x-responsive-nav-link href="{{ route('logout') }}"
-                                   @click.prevent="$root.submit();">
+                                           @click.prevent="$root.submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
                 </form>
-
             </div>
-
-
         </div>
     </div>
 </nav>
