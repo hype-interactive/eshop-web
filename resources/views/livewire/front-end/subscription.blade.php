d627d3a1a3cc36d0e483c41c8b8f3a4f64c1001b
mwagenipecy <percyegno@gmail.com>
2024-07-20 04:51:06 +0300

    update 20-july profile,payment,cart

--- resources/views/livewire/front-end/subscription.blade.php
+++ resources/views/livewire/front-end/subscription.blade.php
@@ -18,54 +18,148 @@
             <div class="flex flex-wrap justify-center justify-between ">
 
                 <!-- Basic Plan -->
-                <div class="w-full md:w-1/3 bg-white rounded-lg shadow p-6">
+                <div class="w-full md:w-1/3 bg-white rounded-3xl  border-1 shadow p-6">
                     <h3 class="text-xl font-bold mb-4">Basic</h3>
                     <p class="text-gray-600 mb-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
                     <p class="text-4xl font-bold mb-4">Tsh 1000 <span class="text-lg font-normal">/mo</span></p>
                     <h4 class="text-lg font-bold mb-2">What's included:</h4>
                     <ul class="mb-6">
-                        <li class="flex items-center mb-2"><span class="text-green-500 mr-2">✔</span> List up to 50 products</li>
-                        <li class="flex items-center mb-2"><span class="text-green-500 mr-2">✔</span> Advanced Statistic</li>
-                        <li class="flex items-center mb-2"><span class="text-green-500 mr-2">✔</span> Basic support</li>
-                        <li class="flex items-center mb-2"><span class="text-green-500 mr-2">✔</span> Access to vendor dashboard</li>
-                        <li class="flex items-center mb-2 text-gray-400"><span class="mr-2">✘</span> Advanced analytics/reporting</li>
-                        <li class="flex items-center mb-2 text-gray-400"><span class="mr-2">✘</span> Featured vendor placement</li>
+                        <li class="flex items-center mb-2"><svg data-slot="icon" fill="#189009" class=" w-6 h-6 text-[#F5AD42] "
+                            stroke-width="1.5" stroke="white" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
+                        </svg> List up to 50 products</li>
+                        <li class="flex items-center mb-2"><svg data-slot="icon" fill="#189009" class=" w-6 h-6 text-[#F5AD42] "
+                            stroke-width="1.5" stroke="white" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
+                        </svg> Advanced Statistic</li>
+                        <li class="flex items-center mb-2"> <svg data-slot="icon" fill="#189009" class=" w-6 h-6 text-[#F5AD42] "
+                            stroke-width="1.5" stroke="white" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
+                        </svg> Basic support</li>
+                        <li class="flex items-center mb-2"> <svg data-slot="icon" fill="#189009" class=" w-6 h-6 text-[#F5AD42] "
+                            stroke-width="1.5" stroke="white" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
+                        </svg> Access to vendor dashboard</li>
+                        <li class="flex items-center mb-2 text-gray-400"><svg data-slot="icon" fill="#189009" class=" w-6 h-6 text-[#F5AD42] "
+                            stroke-width="1.5" stroke="white" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
+                        </svg> Advanced analytics/reporting</li>
+                        <li class="flex items-center mb-2 text-gray-400"><svg data-slot="icon" fill="#189009" class=" w-6 h-6 text-[#F5AD42] "
+                            stroke-width="1.5" stroke="white" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
+                        </svg> Featured vendor placement</li>
                     </ul>
-                    <button class="bg-blue-500 text-white px-4 py-2 rounded-md w-full">Chose Plan</button>
+                    <button class="bg-white text-[#1C70CD] border font-bold  px-4 py-2 rounded-md w-full">Chose Plan</button>
                 </div>
 
                 <!-- Standard Plan -->
-                <div class="w-full md:w-1/3 bg-white rounded-lg shadow p-6 border-4 border-blue-500">
-                    <h3 class="text-xl font-bold mb-4">Standard</h3>
-                    <p class="text-gray-600 mb-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                    <p class="text-4xl font-bold mb-4">Tsh 6000 <span class="text-lg font-normal">/mo</span></p>
-                    <h4 class="text-lg font-bold mb-2">What's included:</h4>
-                    <ul class="mb-6">
-                        <li class="flex items-center mb-2"><span class="text-green-500 mr-2">✔</span> List up to 200 products</li>
-                        <li class="flex items-center mb-2"><span class="text-green-500 mr-2">✔</span> Advanced Statistic</li>
-                        <li class="flex items-center mb-2"><span class="text-green-500 mr-2">✔</span> Priority support</li>
-                        <li class="flex items-center mb-2"><span class="text-green-500 mr-2">✔</span> Access to vendor dashboard</li>
-                        <li class="flex items-center mb-2 text-gray-400"><span class="mr-2">✘</span> Lorem-ipsum dolor-sit amet</li>
-                        <li class="flex items-center mb-2 text-gray-400"><span class="mr-2">✘</span> Lorem-ipsum dolor-sit amet</li>
+                <div class="w-full md:w-1/3 bg-[#1C70CD] -mt-6 mb-6 rounded-3xl  shadow p-6 border-2 border-[#F5AD42]">
+                    <h3 class="text-xl text-white font-bold mb-4">Standard</h3>
+                    <p class="text-gray-600 text-white mb-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    <p class="text-4xl text-white font-bold mb-4">Tsh 6000 <span class="text-lg font-normal">/mo</span></p>
+                    <h4 class="text-lg text-white font-bold mb-2">What's included:</h4>
+                    <ul class="mb-6 ">
+                        <li class="flex items-center text-white mb-2">    <svg data-slot="icon" fill="#189009" class=" w-6 h-6 text-[#F5AD42] "
+                            stroke-width="1.5" stroke="white" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
+                        </svg>
+
+                        List up to 200 products</li>
+                        <li class="flex items-center text-white mb-2"> <svg data-slot="icon" fill="#189009" class=" w-6 h-6 text-[#F5AD42] "
+                            stroke-width="1.5" stroke="white" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
+                        </svg> Advanced Statistic</li>
+                        <li class="flex items-center text-white mb-2"> <svg data-slot="icon" fill="#189009" class=" w-6 h-6 text-[#F5AD42] "
+                            stroke-width="1.5" stroke="white" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
+                        </svg> Priority support</li>
+                        <li class="flex items-center text-white mb-2"> <svg data-slot="icon" fill="#189009" class=" w-6 h-6 text-[#F5AD42] "
+                            stroke-width="1.5" stroke="white" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
+                        </svg> Access to vendor dashboard</li>
+                        <li class="flex items-center text-white mb-2 "> <svg data-slot="icon" fill="#189009" class=" w-6 h-6 text-[#F5AD42] "
+                            stroke-width="1.5" stroke="white" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
+                        </svg> Lorem-ipsum dolor-sit amet</li>
+                        <li class="flex items-center text-white  mb-2  "> <svg data-slot="icon" fill="#189009" class=" w-6 h-6 text-[#F5AD42] "
+                            stroke-width="1.5" stroke="white" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
+                        </svg> Lorem-ipsum dolor-sit amet</li>
                     </ul>
-                    <button class="bg-blue-500 text-white px-4 py-2 rounded-md w-full">Chose Plan</button>
+                    <div class="flex">
+                    <button class="bg-white  text-[#1C70CD] px-4 py-4 item-center justify-center rounded-3xl w-full font-bold  ">Chose Plan</button>
+                    </div>
                 </div>
 
                 <!-- Premium Plan -->
-                <div class="w-full md:w-1/3 bg-white rounded-lg shadow p-6">
+                <div class="w-full md:w-1/3 bg-white rounded-3xl  border-1 shadow p-6">
                     <h3 class="text-xl font-bold mb-4">Premium</h3>
                     <p class="text-gray-600 mb-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
                     <p class="text-4xl font-bold mb-4">Tsh 2100 <span class="text-lg font-normal">/mo</span></p>
                     <h4 class="text-lg font-bold mb-2">What's included:</h4>
                     <ul class="mb-6">
-                        <li class="flex items-center mb-2"><span class="text-green-500 mr-2">✔</span> Unlimited product listings</li>
-                        <li class="flex items-center mb-2"><span class="text-green-500 mr-2">✔</span> Dedicated account manager</li>
-                        <li class="flex items-center mb-2"><span class="text-green-500 mr-2">✔</span> Access to vendor dashboard</li>
-                        <li class="flex items-center mb-2"><span class="text-green-500 mr-2">✔</span> Advanced analytics/reporting</li>
-                        <li class="flex items-center mb-2"><span class="text-green-500 mr-2">✔</span> Featured vendor placement</li>
-                        <li class="flex items-center mb-2"><span class="text-green-500 mr-2">✔</span> Directory Listing</li>
+                        <li class="flex items-center mb-2"> <svg data-slot="icon" fill="#189009" class=" w-6 h-6 text-[#F5AD42] "
+                            stroke-width="1.5" stroke="white" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
+                        </svg> Unlimited product listings</li>
+                        <li class="flex items-center mb-2"> <svg data-slot="icon" fill="#189009" class=" w-6 h-6 text-[#F5AD42] "
+                            stroke-width="1.5" stroke="white" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
+                        </svg> Dedicated account manager</li>
+                        <li class="flex items-center mb-2"> <svg data-slot="icon" fill="#189009" class=" w-6 h-6 text-[#F5AD42] "
+                            stroke-width="1.5" stroke="white" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
+                        </svg> Access to vendor dashboard</li>
+                        <li class="flex items-center mb-2"> <svg data-slot="icon" fill="#189009" class=" w-6 h-6 text-[#F5AD42] "
+                            stroke-width="1.5" stroke="white" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
+                        </svg> Advanced analytics/reporting</li>
+                        <li class="flex items-center mb-2"><svg data-slot="icon" fill="#189009" class=" w-6 h-6 text-[#F5AD42] "
+                            stroke-width="1.5" stroke="white" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
+                        </svg> Featured vendor placement</li>
+                        <li class="flex items-center mb-2"><svg data-slot="icon" fill="#189009" class=" w-6 h-6 text-[#F5AD42] "
+                            stroke-width="1.5" stroke="white" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
+                        </svg> Directory Listing</li>
                     </ul>
-                    <button class="bg-blue-500 text-white px-4 py-2 rounded-md w-full">Chose Plan</button>
+                    <button class="bg-white  text-[#1C70CD]  border font-bold  px-4 py-2 rounded-md w-full">Chose Plan</button>
                 </div>
             </div>
         </div>
