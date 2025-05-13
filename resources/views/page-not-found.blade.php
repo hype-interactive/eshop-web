dd3086bcc658f62d7d8a9effeb0d6896f71d4fc9
mwagenipecy <percyegno@gmail.com>
2025-05-13 18:42:21 +0300

    fixed

--- resources/views/page-not-found.blade.php
+++ resources/views/page-not-found.blade.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>eShops</title>
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     @vite('resources/css/app.css')
     <script src="//unpkg.com/alpinejs" defer></script>
 
@@ -31,7 +30,6 @@
     </main>
 
 
-    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 
 </html>
